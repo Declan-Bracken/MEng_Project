b30a55dfb1a257db1c1897c19254b02f8d8cdc9b
Deklanor <declankbracken@gmail.com>
2024-05-16 11:08:05 -0400

    Removed dataframe processor from pipeline and added streamlit cacheing to cluster tuning, among other changes.

--- TableReconstruction/column_clusterer.py
+++ TableReconstruction/column_clusterer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics import pairwise_distances
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import davies_bouldin_score
@@ -37,7 +38,6 @@ class ColumnClusterer:
         return labels
 
     def binary_search_optimize_eps(self, positions, eps_min=5, eps_max=50, tolerance=1):
-
         best_eps = None
         best_dbi = float('inf')
 
@@ -66,6 +66,48 @@ class ColumnClusterer:
             return eps_mid
         
         return best_eps
+    
+    def process_tables_to_dataframe(self, tables_data, min_samples_list):
+        all_tables_dfs = []
+
+        for idx, table_data in enumerate(tables_data):
+            min_samples = min_samples_list[idx]  # Get the min_samples for the current table
+
+            positions = table_data['positions']
+            texts = table_data['texts']
+            line_numbers = table_data['line_numbers']
+
+            num_lines = len(set(line_numbers))
+
+            # Check for more than one word per row:
+            has_columns = len(line_numbers) != num_lines
+
+            if has_columns and num_lines > 1:
+                labels = self.perform_hdbscan_clustering(positions, min_cluster_size=num_lines, min_samples=min_samples)
+                labels = self.reorder_columns(labels, positions)
+                num_cols = len(set(labels))
+            elif num_lines > 1:  # More than one line, but one column
+                labels = [0 for _ in range(num_lines)]
+                num_cols = len(set(labels))
+            else:  # More than one column, but one line
+                optimal_eps = 27.5
+                labels = self.perform_clustering(positions, optimal_eps)
+                num_cols = len(set(labels))
+
+            df = pd.DataFrame(np.nan, index=range(num_lines), columns=range(num_cols))
+
+            for text, label, line in zip(texts, labels, line_numbers):
+                if 0 <= label < num_cols and 0 <= line < num_lines:
+                    if pd.notna(df.at[line, label]):
+                        df.at[line, label] = f"{df.at[line, label]} {text}"
+                    else:
+                        df.at[line, label] = text
+
+            df.dropna(how='all', inplace=True)
+            df.reset_index(drop=True, inplace=True)
+            all_tables_dfs.append(df)
+
+        return all_tables_dfs
 
 
 class ColumnClustererV2:
--- TableReconstruction/image_processor.py
+++ TableReconstruction/image_processor.py
@@ -1,5 +1,26 @@
 import cv2
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# class ImageProcessor:
+#     def __init__(self, image_path, boxes):
+#         self.image_path = image_path
+#         self.image = cv2.imread(image_path)
+#         if self.image is None:
+#             print(f"Failed to load image from {image_path}.")
+#             return
+#         self.image_rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+#         self.boxes = boxes
+#         self.cropped_images = self.crop_images()
+
+#     def crop_images(self):
+#         cropped_images = []
+#         for box in self.boxes:
+#             x1, y1, x2, y2 = map(int, box[:4])
+#             cropped_image = Image.fromarray(self.image_rgb[y1:y2, x1:x2])
+#             cropped_images.append(cropped_image)
+#         return cropped_images
 
 class ImageProcessor:
     def __init__(self, image_path, boxes):
@@ -16,6 +37,52 @@ class ImageProcessor:
         cropped_images = []
         for box in self.boxes:
             x1, y1, x2, y2 = map(int, box[:4])
-            cropped_image = Image.fromarray(self.image_rgb[y1:y2, x1:x2])
-            cropped_images.append(cropped_image)
+            cropped_image = self.image_rgb[y1:y2, x1:x2]
+            preprocessed_image = self.preprocess_image(cropped_image)
+            cropped_images.append(Image.fromarray(preprocessed_image))
         return cropped_images
+
+    def preprocess_image(self, image):
+        # Apply Gaussian Blur to reduce noise
+        blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+        
+        # Convert to grayscale
+        gray_image = cv2.cvtColor(blurred_image, cv2.COLOR_RGB2GRAY)
+        
+        # Enhance contrast
+        alpha = 1.5 # Simple contrast control [1.0-3.0]
+        beta = 0    # Simple brightness control [0-100]
+        contrast_enhanced_image = cv2.convertScaleAbs(gray_image, alpha=alpha, beta=beta)
+        
+        # Sharpen the image
+        kernel = np.array([[0, -1, 0], [-1, 5,-1], [0, -1, 0]])
+        sharpened_image = cv2.filter2D(contrast_enhanced_image, -1, kernel)
+        
+        # Convert back to RGB (optional, if you want to keep the 3-channel format)
+        processed_image = cv2.cvtColor(sharpened_image, cv2.COLOR_GRAY2RGB)
+        
+        return processed_image
+    
+    def plot_images(self):
+        if not self.cropped_images:
+            print("No images to display.")
+            return
+        
+        num_images = len(self.cropped_images)
+        fig, axes = plt.subplots(1, num_images, figsize=(15, 15))
+        
+        if num_images == 1:
+            axes = [axes]  # Ensure axes is iterable if there's only one image
+        
+        for ax, img in zip(axes, self.cropped_images):
+            ax.imshow(img)
+            ax.axis('off')
+        
+        plt.show()
+
+if __name__ == "__main__":
+    # Example usage:
+    image_path = "/Users/declanbracken/Development/UofT_Projects/Meng_Project/Transcripts/Web_Scraped_Transcripts/transcrip-02102015-1-2048.webp"
+    boxes = [(50, 50, 200, 200), (300, 300, 450, 450)]  # Example bounding boxes
+    processor = ImageProcessor(image_path, boxes)
+    processor.plot_images()
